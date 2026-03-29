750a2980f7aaf0d298dff6adb094944a7f8476fc
twodandy <107325530+jolivier16@users.noreply.github.com>
2026-03-28 21:04:54 -0400

    implmented rolling copyright year

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-  const burger = document.querySelector('.burger');
-  const navBar = document.querySelector('.nav-bar');
-  burger.addEventListener('click', () => {
-    navBar.classList.toggle('open');
+const burger = document.querySelector('.burger');
+const navBar = document.querySelector('.nav-bar');
+burger.addEventListener('click', () => {
+navBar.classList.toggle('open');
   });
+
+const copyrightYear = document.querySelector('.footer-copyright p');
+const currentYear = new Date().getFullYear();
+copyrightYear.textContent = `© ${currentYear} James Olivier, All rights reserved.`;
